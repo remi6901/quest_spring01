93e2c8d942fa2f73b027c2b9426cfc765df1ca9a
remi6901 <descollonges.remi@gmail.com>
2022-11-14 12:06:59 +0100

    create DoctorController

---
+++ src/main/java/com/wildcodeschool/questSpring01/QuestSpring01Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.questSpring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestSpring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestSpring01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/questSpring01/QuestSpring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.questSpring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestSpring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
